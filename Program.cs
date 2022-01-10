e4f94be59d4d042f0e650855f5286aa8d7a3b45e
Bang1710 <87267619+Bang1710@users.noreply.github.com>
2022-01-10 20:02:41 +0700

    .

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProgramC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
